695f010d4a306b39b77123fc6c8757bc9a149d5e
iharuya <i4haruya@gmail.com>
2022-10-08 21:38:40 +0900

    setup erc721 with a script to deploy

--- contract/contracts/Spells.sol
+++ contract/contracts/Spells.sol
@@ -1,8 +1,32 @@
-// SPDX-License-Identifier: UNLICENSED
+// SPDX-License-Identifier: MIT
 pragma solidity ^0.8.17;
 
 import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
+import "@openzeppelin/contracts/access/Ownable.sol";
 
-abstract contract Spells is ERC721 {
-    constructor() {}
+contract Spells is ERC721, Ownable {
+    uint256 private _tokenId = 1;
+    uint256 private immutable _mintPrice;
+    string public baseURI;
+    event Minted(uint256 indexed tokenId, address indexed to, string spell);
+
+    constructor(uint256 mintPrice_) ERC721("Spells", "SPL") {
+        setBaseURI("https://example.com/");
+        _mintPrice = mintPrice_;
+    }
+
+    function addSpell(string memory spell_) public payable {
+        require(msg.value == _mintPrice, "Wrong price");
+        _safeMint(msg.sender, _tokenId);
+        emit Minted(_tokenId, msg.sender, spell_);
+        _tokenId++;
+    }
+
+    function setBaseURI(string memory URI_) public onlyOwner {
+        baseURI = URI_;
+    }
+
+    function _baseURI() internal view override returns (string memory) {
+        return baseURI;
+    }
 }
---
+++ contract/scripts/deploy.ts
@@ -0,0 +1,33 @@
+import { network, ethers } from "hardhat"
+import { networksConfig, sendValues, verify } from "../helper"
+require("dotenv").config()
+
+async function main() {
+  const signers = await ethers.getSigners()
+  const deployer = signers[0]
+  const networkConfigHelper = networksConfig[network.name]
+  console.log("Network:", network.name)
+  console.log("Deployer:", deployer.address)
+
+  if (network.name === "localhost") {
+    await sendValues(signers.map(signer => signer.address))
+  }
+  console.log("Deployer balance:", ethers.utils.formatEther(await deployer.getBalance()).toString())
+
+  const factory = await ethers.getContractFactory("Spells")
+  const mintPrice = ethers.utils.parseEther("0.01")
+  const params = [mintPrice]
+  const contract = await factory.deploy(...params)
+  await contract.deployed()
+  await contract.deployTransaction.wait(networkConfigHelper?.confirmations || 1)
+  console.log("Contract deployed to:", contract.address)
+
+  if (networkConfigHelper?.verify) {
+    await verify(contract.address, params)
+  }
+}
+
+main().catch((err) => {
+  console.error(err)
+  process.exitCode = 1
+})
