78b8f5290fa17994aa2a8a07817d5bf1a19b714e
iharuya <i4haruya@gmail.com>
2022-10-16 23:18:44 +0900

    introduce siwe (partial)

--- app/pages/api/auth/[...nextauth].ts
+++ app/pages/api/auth/[...nextauth].ts
@@ -1,16 +1,77 @@
 import NextAuth, { NextAuthOptions } from "next-auth"
 import GithubProvider from "next-auth/providers/github"
+import CredentialsProvider from "next-auth/providers/credentials"
+import type { NextApiRequest, NextApiResponse } from "next"
+import { getCsrfToken } from "next-auth/react"
+import { SiweMessage } from "siwe"
+
 import { PrismaAdapter } from "@next-auth/prisma-adapter"
 import prisma from "@/lib/prismadb"
 
-const authOptions: NextAuthOptions = {
-  adapter: PrismaAdapter(prisma),
-  providers: [
-    GithubProvider({
-      clientId: process.env.GITHUB_ID as string,
-      clientSecret: process.env.GITHUB_SECRET as string,
-    }),
-  ],
+const auth = async (req: NextApiRequest, res: NextApiResponse) => {
+  const authOptions: NextAuthOptions = {
+    adapter: PrismaAdapter(prisma),
+    providers: [
+      GithubProvider({
+        clientId: process.env.GITHUB_ID as string,
+        clientSecret: process.env.GITHUB_SECRET as string,
+      }),
+      CredentialsProvider({
+        name: "Ethereum",
+        credentials: {
+          message: {
+            label: "Message",
+            type: "text",
+          },
+          signature: {
+            label: "Signature",
+            type: "text",
+          },
+        },
+        async authorize(credentials) {
+          try {
+            const nextAuthUrl = process.env.NEXTAUTH_URL
+            if (!nextAuthUrl) return null
+            if (!credentials) return null
+            const message = new SiweMessage(JSON.parse(credentials.message))
+            const nextAuthHost = new URL(nextAuthUrl).host
+            if (message.domain !== nextAuthHost) return null
+            if (message.nonce !== (await getCsrfToken({ req }))) return null
+
+            const result = await message.verify({
+              signature: credentials.signature,
+            })
+            if (!result.success) return null
+            return {
+              id: message.address,
+            }
+          } catch (e) {
+            console.error(e)
+            return null
+          }
+        },
+      }),
+    ],
+
+    callbacks: {
+      // async session({ session, token, user }) {
+      //   session.address = token.sub
+      //   return session
+      // },
+    },
+
+    secret: process.env.NEXTAUTH_SECRET,
+  }
+
+  // Hide Sign-In with Ethereum from default sign page
+  const isDefaultSigninPage =
+    req.method === "GET" && req.query.nextauth?.includes("signin")
+  if (isDefaultSigninPage) {
+    authOptions.providers.pop()
+  }
+
+  const ret = await NextAuth(req, res, authOptions)
+  return ret
 }
 
-export default NextAuth(authOptions)
+export default auth
--- app/pages/index.tsx
+++ app/pages/index.tsx
@@ -1,29 +1,77 @@
 import type { NextPage } from "next"
-import { useAccount } from "wagmi"
-import { useSession, signIn, signOut } from "next-auth/react"
+import { useSession, signIn, signOut, getCsrfToken } from "next-auth/react"
+import { useAccount, useNetwork, useSignMessage } from "wagmi"
+import { SiweMessage } from "siwe"
 
 const Page: NextPage = () => {
-  const { address: connectedAddress } = useAccount()
   const { data: session, status } = useSession()
+  const { address: connectedAddress } = useAccount()
+  const { chain: connectedChain } = useNetwork()
+  const { signMessageAsync } = useSignMessage()
+
+  const handleLogin = async () => {
+    try {
+      const now = new Date()
+      const expiration = new Date()
+      expiration.setHours(now.getHours() + 1)
+      const message = new SiweMessage({
+        domain: window.location.host,
+        address: connectedAddress,
+        statement: "ウォレットでログインします",
+        uri: window.location.origin,
+        version: "1",
+        chainId: connectedChain?.id,
+        nonce: await getCsrfToken(),
+        issuedAt: now.toISOString(),
+        expirationTime: expiration.toISOString(),
+      })
+      const humanMessage = message.prepareMessage()
+      const signature = await signMessageAsync({ message: humanMessage })
+      signIn("credentials", {
+        message: JSON.stringify(message),
+        redirect: false,
+        callbackUrl: "/protected",
+        signature,
+      })
+    } catch (error) {
+      window.alert(error)
+    }
+  }
 
   return (
     <>
-      <p>{process.env.NODE_ENV}</p>
-      <p>{connectedAddress}</p>
-      {status === "authenticated" ? (
-        <div>
-          <p>こんにちは、{session.user?.email}</p>
-          <button onClick={() => signOut()} className="btn btn-outline">
-            ログアウト
-          </button>
-        </div>
-      ) : (
-        <div>
-          <button onClick={() => signIn()} className="btn btn-primary">
-            ログイン
-          </button>
-        </div>
-      )}
+      <p>環境：{process.env.NODE_ENV}</p>
+      <p>接続アドレス：{connectedAddress}</p>
+      <div className="my-3">
+        {status === "authenticated" ? (
+          <>
+            <p>ログイン中</p>
+            <pre>
+              <code>{JSON.stringify(session, null, 2)}</code>
+            </pre>
+            <button onClick={() => signOut()} className="btn btn-outline">
+              ログアウト
+            </button>
+          </>
+        ) : (
+          <>
+            <button onClick={() => signIn()} className="btn">
+              普通にログイン
+            </button>
+          </>
+        )}
+      </div>
+      <div className="my-3">
+        <button
+          onClick={(e) => {
+            e.preventDefault()
+            handleLogin()
+          }}
+          className="btn"
+        >
+          ウォレットでログイン
+        </button>
+      </div>
     </>
   )
 }
